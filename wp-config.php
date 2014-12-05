b63eb0f59161c198d041c55844d18b03d7da57a4
fanzhanao <fanzhanao@modernmedia.com.cn>
2014-12-05 16:35:27 +0800

    修改密码

--- wp-config.php
+++ wp-config.php
@@ -21,7 +21,7 @@ define('DB_NAME', 'wordpress');
 define('DB_USER', 'root');
 
 /** MySQL数据库密码 */
-define('DB_PASSWORD', '123456');
+define('DB_PASSWORD', 'modernmedia');
 
 /** MySQL主机 */
 define('DB_HOST', 'localhost');
