b0d3cc1041dda5ec8c8127750ce78dc1cfce99f3
fanzhanao <fanzhanao@modernmedia.com.cn>
2014-12-10 14:04:58 +0800

    添加顶部菜单

--- wp-content/themes/icalendar/header.php
+++ wp-content/themes/icalendar/header.php
@@ -47,12 +47,11 @@
 			<div class="search-toggle">
 				<a href="#search-container" class="screen-reader-text"><?php _e( 'Search', 'twentyfourteen' ); ?></a>
 			</div>
-            <!--
 			<nav id="primary-navigation" class="site-navigation primary-navigation" role="navigation">
 				<button class="menu-toggle"><?php //_e( 'Primary Menu', 'twentyfourteen' ); ?></button>
 				<a class="screen-reader-text skip-link" href="#content"><?php _e( 'Skip to content', 'twentyfourteen' ); ?></a>
 				<?php //wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
-			</nav>-->
+			</nav>
 		</div>
 
 		<div id="search-container" class="search-box-wrapper hide">
