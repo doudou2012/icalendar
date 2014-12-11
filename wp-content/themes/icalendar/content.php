e01409e8a57536d99c4ee169fca2539489a47bf8
root <root@web-server-12.novalocal>
2014-12-11 15:45:51 +0800

    去掉内容页的显示判断,去掉CSS样式的Print部分

--- wp-content/themes/icalendar/content.php
+++ wp-content/themes/icalendar/content.php
@@ -56,12 +56,7 @@
 	<div class="entry-content">
 		<?php
             //展览类型，取自定义字段的值
-            if ( get_post_type() === 'event'){
-                echo types_render_field('description', array('output'=>'html'));
-            }
-            else{
-                the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentyfourteen' ) );
-            }
+            the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentyfourteen' ) );
 
 //			wp_link_pages( array(
 //				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentyfourteen' ) . '</span>',
