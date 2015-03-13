e9f39804a04fe9fc79a60065bf884e322af48b8c
fanzhanao <94524900@qq.com>
2015-03-13 19:17:14 +0800

    增加置顶功能

--- js/sticky.js
+++ js/sticky.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
 			$('table').find('tr').append($('<label class="alignleft"><input type="checkbox" name="sticky" value="sticky"> <span class="checkbox-title">置顶</span></label>'));
 			$('input[name="sticky"]').on('click',function(){
 				var sticky = $(this).is(':checked') ? 'sticky':false;
-					pid = $(this).parent().find('td:first').text();
-				$.post(StickyAjax.ajaxurl,{"action":"my_action","security":StickyAjax.security,"sticky":sticky,"post_ID":328},function(data){
+					pid = $(this).parent().parent().find('td:first').text();
+				$.post(StickyAjax.ajaxurl,{"action":"my_action","security":StickyAjax.security,"sticky":sticky,"post_ID":pid},function(data){
 					console.log(data);
 				});
 			});
