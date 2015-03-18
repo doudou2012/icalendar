f8230b0eb09493e1feee499a161a5a713e0860cf
fanzhanao <94524900@qq.com>
2015-03-18 18:01:31 +0800

    增加添加收藏功能

--- js/icalendar.js
+++ js/icalendar.js
@@ -11,33 +11,47 @@
     };
     var setDisabled = function(){
         $('#add-fav').attr("disabled", "disabled");
-        $('#add-fav').css({"background-color":"#666","opacity":.65,"pointer-events":"none","cursor":"not-allowed"});
+        $('#add-fav').css({"pointer-events":"none","cursor":"not-allowed"});
         $('#add-fav').text('已收藏');
     };
     var showloginForm = function(){
         var sign = new Sign({login_success:function(){window.location.reload()}});
         sign.login_dialog();
     };
-    var addFav = function(pid){
+    /*添加收藏*/
+    var addFavorite = function(pid){
+        var url = baseUrl + '?favorite&add';
+        $.getJSON(url, {postid: pid}, function (data) {
+            if (data.success) {
+                setDisabled();
+            } else if (data.error.code == 'not_login') {
+                showloginForm();
+            } else {
+                alertify.alert('收藏失败:' + data.error.msg).set('basic', true);
+                setTimeout(function () {
+                    alertify.close();
+                }, 3000);
+            }
+        });
+
+    };
+    /*检查是否已经收藏*/
+    var checkFav = function(pid){
         if ($('#add-fav').length > 0){
             if (!pid || parseInt(pid) <=0) {
-                pid = $('article:first').attr('id').replace(/[^\d]/g, '');
+                pid = parseInt($('article:first').attr('id').replace(/[^\d]/g, ''));
             }
-            $('#add-fav').on('click', function () {
-                var url = baseUrl + '?favorite&add';
-                $.getJSON(url, {postid: pid}, function (data) {
-                    if (data.success) {
+            if (pid > 0 ){
+                $.getJSON(baseurl+'?favorite&check_fav',{postid:pid},function(data){
+                    if (data.success){
                         setDisabled();
-                    } else if (data.error.code == 'not_login') {
-                        showloginForm();
-                    } else {
-                        alertify.alert('收藏失败:' + data.error.msg).set('basic', true);
-                        setTimeout(function () {
-                            alertify.close();
-                        }, 3000);
+                    }else{
+                        $('#add-fav').on('click', function () {
+                            addFavorite(pid);
+                        });
                     }
                 });
-            });
+            }
         }else{
             return false;
         }
@@ -57,6 +71,6 @@
                 shared($(document).find('title').text(),location.href,'');
             });
         }
-        addFav();
+        checkFav(0);
     });
 })(jQuery);
