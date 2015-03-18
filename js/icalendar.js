9f4d5bf0cb3aa265e35bb341ff6ff82a1704386e
fanzhanao <94524900@qq.com>
2015-03-18 15:47:56 +0800

    修改文章详情页面

--- js/icalendar.js
+++ js/icalendar.js
@@ -2,6 +2,7 @@
  * Created by user on 15/3/17.
  */
 (function($){
+    $('.site-main').css('margin-top','44px');
     var baseUrl = window.location.pathname;
     var shared = function(content,url,img){
         var param = {"content":content,"url":url,"image":img};
@@ -9,11 +10,23 @@
             console.log(result);
         });
     };
+
     $(document).ready(function(){
-        shared($(document).find('title').text(),location.href,'');
         if ($('.icalendar-slider').length > 0 ){
             $('.flexslider').css("margin","0");
             $('.flexslider').flexslider({"smoothHeight":true,"slideshowSpeed":3000,"controlNav":false,"directionNav":false});
         }
+
+        if ($('.nav-back').length > 0){
+            $('.nav-back').on('click',function(){
+                history.go(-1);
+            });
+        }
+        if ($('.nav-share').length > 0){
+            $('.nav-share').on('click',function(){
+                shared($(document).find('title').text(),location.href,'');
+            });
+        }
+        //$('')
     });
 })(jQuery);
