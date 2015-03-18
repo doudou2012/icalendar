83ebe4b4758d78d2e85c4e61df741a3f192802a8
fanzhanao <94524900@qq.com>
2015-03-18 15:58:22 +0800

    修改文章详情页面

--- header-mobile-single.php
+++ header-mobile-single.php
@@ -31,6 +31,6 @@
             </div>
         </header>
     <?php endif;?>
-    <div id="main" class="site-main">
+    <div id="main" class="site-main<?php if (ua_icalendar_app() && is_single()) echo ' nav-margin-top'; ?>">
 
 
