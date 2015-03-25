6978cf9f01185467c4c4278c9846199b92b7cdc8
fanzhanao <94524900@qq.com>
2015-03-25 11:25:08 +0800

    修正首页轮播

--- footer-mobile.php
+++ footer-mobile.php
@@ -18,10 +18,11 @@
 ?>
         <script type="text/javascript" src="<?=WP_PLUGIN_URL?>/wxrobot/static/alertifyjs/alertify.min.js"></script>
         <script type="text/javascript" src="<?=WP_PLUGIN_URL?>/wxrobot/account/static/sign.js"></script>
-        <script type='text/javascript' src='<?php echo get_template_directory_uri();?>/js/jquery.flexslider.js'></script>
         <script type='text/javascript' src='<?php echo get_template_directory_uri();?>/js/webridge.js'></script>
         <script type='text/javascript' src='<?php echo get_template_directory_uri();?>/js/layer/layer.min.js'></script>
         <script type='text/javascript' src='<?php echo get_template_directory_uri();?>/js/icalendar.js'></script>
+        <script type='text/javascript' src='<?php echo get_template_directory_uri();?>/js/wowslider/wowslider.js'></script>
+        <script type='text/javascript' src='<?php echo get_template_directory_uri();?>/js/wowslider/script.js'></script>
     <?php endif;?>
  <script type="text/javascript">
  $(document).ready(function(){
