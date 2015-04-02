4f86c96a20ac1449b89f08eae0fcc413877f13b8
fanzhanao <94524900@qq.com>
2015-04-02 18:04:20 +0800

    添加缓存

--- header-mobile-single.php
+++ header-mobile-single.php
@@ -7,7 +7,7 @@
 */
 ?>
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html manifest="<?=home_url()?>/manifest.php">
     <head>
         <meta charset="<?php bloginfo( 'charset' ); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
