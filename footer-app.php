c0d1c1e1ac6a8f36930af55141ec439e6786f5fa
fanzhanao <94524900@qq.com>
2015-04-09 18:16:15 +0800

    修改头部应用脚本

--- footer-app.php
+++ footer-app.php
@@ -1,13 +1,12 @@
 </div><!-- #main -->
 </div><!-- #page -->
 <script type='text/javascript' src='http://libs.baidu.com/jquery/2.0.0/jquery.min.js'></script>
-<script type='text/javascript' src='<?php echo get_template_directory_uri();?>/js/iscroll-infinite.js'></script>
 <script type='text/javascript' src='<?php echo get_template_directory_uri();?>/js/functions.js'></script>
 <script type="text/javascript" src="<?=WP_PLUGIN_URL?>/wxrobot/static/alertifyjs/alertify.min.js"></script>
 <script type="text/javascript" src="<?=WP_PLUGIN_URL?>/wxrobot/account/static/sign.js"></script>
-<script type='text/javascript' src='<?php echo get_template_directory_uri();?>/js/webridge.js'></script>
 <script type='text/javascript' src='<?php echo get_template_directory_uri();?>/js/layer/layer.min.js'></script>
 <script type='text/javascript' src='<?php echo get_template_directory_uri();?>/js/layer/extend/layer.ext.js'></script>
+<script type='text/javascript' src='<?php echo get_template_directory_uri();?>/js/jquery.excoloSlider.min.js'></script>
 <script type='text/javascript' src='<?php echo get_template_directory_uri();?>/js/icalendar.js'></script>
 </body>
 </html>
--- header-app.php
+++ header-app.php
@@ -6,10 +6,12 @@
     <link rel='stylesheet' id='genericons-css'  href='<?php echo get_template_directory_uri();?>/genericons/genericons.css' type='text/css' media='all' />
     <link rel='stylesheet' id='twentyfourteen-style-css'  href='<?php echo get_template_directory_uri();?>/style-mobile.css' />
     <link href="http://cdn.bootcss.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/css/icalendar-app.css" rel="stylesheet" />
     <link rel="stylesheet" href="<?=WP_PLUGIN_URL?>/wxrobot/static/alertifyjs/css/alertify.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="<?=WP_PLUGIN_URL?>/wxrobot/static/alertifyjs/css/themes/bootstrap.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="<?=WP_PLUGIN_URL?>/wxrobot/static/css/alert-style.css" rel="stylesheet" />
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/css/icalendar-app.css" rel="stylesheet" />
+    <link rel='stylesheet' id='slider-style-css'  href="<?php echo get_template_directory_uri();?>/css/gxy-single-event.css" type='text/css' media='all' />
+    <link rel='stylesheet' id='slider-style-css'  href="<?php echo get_template_directory_uri();?>/css/jquery.excoloSlider.css" type='text/css' media='all' />
     <script type="text/javascript">
         //        layer.use('extend/layer.ext.js');
     </script>
