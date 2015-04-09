0c5b5e2ec57179e0c643eddd0633de338afba38d
fanzhanao <94524900@qq.com>
2015-04-09 11:28:35 +0800

    修改头部样式

--- header-app.php
+++ header-app.php
@@ -2,7 +2,7 @@
 <html manifest="<?=home_url().'/manifest.php'?>">
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no">
     <link rel='stylesheet' id='genericons-css'  href='<?php echo get_template_directory_uri();?>/genericons/genericons.css' type='text/css' media='all' />
     <link rel='stylesheet' id='twentyfourteen-style-css'  href='<?php echo get_template_directory_uri();?>/style-mobile.css' />
     <link href="http://cdn.bootcss.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet">
