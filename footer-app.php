4540d6059e9f8258fa9239e26f0fb02b6884c9f4
fanzhanao <94524900@qq.com>
2015-04-09 18:32:15 +0800

    修改头部引用的样式

--- footer-app.php
+++ footer-app.php
@@ -4,6 +4,7 @@
 <script type='text/javascript' src='<?php echo get_template_directory_uri();?>/js/functions.js'></script>
 <script type="text/javascript" src="<?=WP_PLUGIN_URL?>/wxrobot/static/alertifyjs/alertify.min.js"></script>
 <script type="text/javascript" src="<?=WP_PLUGIN_URL?>/wxrobot/account/static/sign.js"></script>
+<script type='text/javascript' src='<?php echo get_template_directory_uri();?>/js/webridge.js'></script>
 <script type='text/javascript' src='<?php echo get_template_directory_uri();?>/js/layer/layer.min.js'></script>
 <script type='text/javascript' src='<?php echo get_template_directory_uri();?>/js/layer/extend/layer.ext.js'></script>
 <script type='text/javascript' src='<?php echo get_template_directory_uri();?>/js/jquery.excoloSlider.min.js'></script>
