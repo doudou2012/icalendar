f1f3941df54fff8f2a99364735523f4615d60a92
Xiaoyu Guan <kwanxy@gmail.com>
2015-04-09 23:05:45 +0800

    显示/隐藏

--- content-invite-single.php
+++ content-invite-single.php
@@ -19,13 +19,13 @@ get_template_part('header','app');
 ?>
 <article id="post-<?=$post->ID?>" <?php post_class(); ?>>
     <header class="entry-header">
-        <h1 class="entry-title" style="font-weight: 300;">
+        <h1 class="entry-title" style="font-weight: 300; font-size: 20px;">
         <?php echo '我想去看在「'.types_render_field('place','').'」举办的「'.$post->post_title.'」展览，你想一起来吗？'; ?>
         </h1>
         <div class="all-sorts-of-meta">
-            <ul>
+            <ul class="list-inline">
                 <li>时间：<?=$date?></li>
-                <li>邀请人：<span class="glyphicon glyphicon-user" style=color:gray><?=$current_user->login?></span> <?=$current_user->user_login?></li>
+                <li>邀请人：<?=$current_user->login?> <?=$current_user->user_login?></li>
             </ul>
             <button class="btn btn-default btn-not-rounded btn-sm" id="detailTrigger" onclick="showHideEventDetail()">展览详情 <span class="caret"></span></button>
         </div>
@@ -115,21 +115,21 @@ get_template_part('header','app');
         <div>
             <button class="btn btn-lg color-red btn-not-rounded btn-block" id="accept-invite">报名参与</button>
         </div>
-        <?php
-        echo the_join_list($post->ID,false);
-        get_template_part('footer','app');
-        ?>
-        <script type="text/javascript">
-            function showHideEventDetail () {
-                var triggerNode = document.getElementById('detailTrigger');
-                var targetNode = document.getElementById('eventDetail');
-                if ( targetNode.classList.toString().indexOf('hidden') == -1 ) {
-                    targetNode.classList.add('hidden');
-                    triggerNode.lastChild.classList.remove('rotate-180');
-                } else {
-                    targetNode.classList.remove('hidden')
-                    triggerNode.lastChild.classList.add('rotate-180');
-                }
-            }
-        </script>
     </article><!-- #post-## -->
+    <?php
+    echo the_join_list($post->ID,false);
+    get_template_part('footer','app');
+    ?>
+    <script type="text/javascript">
+        function showHideEventDetail () {
+            var triggerNode = document.getElementById('detailTrigger');
+            var targetNode = document.getElementById('eventDetail');
+            if ( targetNode.classList.toString().indexOf('hidden') == -1 ) {
+                targetNode.classList.add('hidden');
+                triggerNode.lastChild.classList.remove('rotate-180');
+            } else {
+                targetNode.classList.remove('hidden')
+                triggerNode.lastChild.classList.add('rotate-180');
+            }
+        }
+    </script>
